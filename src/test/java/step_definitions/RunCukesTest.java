a1bcf1d5b2631bca97755b6553642962f0bb4d7d
ghalda <gputribalqis@gmail.com>
2023-07-04 09:50:43 +0700

    fixing tags

--- src/test/java/step_definitions/RunCukesTest.java
+++ src/test/java/step_definitions/RunCukesTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        tags = {"@WebTest"},
+        tags = {"@Register"},
         features = {"classpath:features/"},
         glue = {"classpath:step_definitions"},
         plugin = {"pretty", "html:/target/site/cucumber-pretty","json:target/cucumber/cucumber.json"}
